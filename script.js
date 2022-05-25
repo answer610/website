5683a9f42d3a462933b01ea928b36abb2132a762
An Le <anl2002@berkeley.edu>
2022-05-24 23:59:36 -0700

    Remove WCA logo, use cube logo instead

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ function lightMode() {
     var target1 = document.body;
     target1.classList.toggle("dark-mode");
     if (document.getElementById("switch").checked === true) {
-        document.getElementById("switch").backgroundImage = "images/sun.jpeg"
+        document.getElementById("switch").backgroundImage = "images/sun.jpeg";
         document.getElementById("Resume").src = "images/resume_black.png";
         document.getElementById("GitHub").src = "images/github_black.png";
+        document.getElementById("wca").src = "images/rubik.png";
         document.getElementById("tab1").style.color = "black";
         document.getElementById("tab2").style.color = "black";
     } else {
-        document.getElementById("switch").backgroundImage = "images/moon.jpeg"
+        document.getElementById("switch").backgroundImage = "images/moon.jpeg";
         document.getElementById("Resume").src = "images/resumelogo.png";
         document.getElementById("GitHub").src = "images/githublogo2.png";
+        document.getElementById("wca").src = "images/rubik_dark.png";
         document.getElementById("tab1").style.color = "white";
         document.getElementById("tab2").style.color = "white";
     }
