439f6bec06a1418e85f1b88d7b37ab1a4771ff08
An Le <anl2002@berkeley.edu>
2022-05-25 23:09:25 -0700

    Replace tabs with dropdown menu, add email icon, add dark-light mode for projects tab

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-function lightMode() {
+function lightModeIndex() {
     var target1 = document.body;
     target1.classList.toggle("dark-mode");
     if (document.getElementById("switch").checked === true) {
         document.getElementById("switch").backgroundImage = "images/sun.jpeg";
         document.getElementById("Resume").src = "images/resume_black.png";
+        document.getElementById("Mail").src = "images/mail_white.png";
         document.getElementById("GitHub").src = "images/github_black.png";
         document.getElementById("wca").src = "images/rubik.png";
         document.getElementById("tab1").style.color = "black";
         document.getElementById("tab2").style.color = "black";
+        document.getElementById("button").style.backgroundImage = "url('images/menu_black.png')";
+        document.getElementById("button").style.backgroundColor = "white";
     } else {
         document.getElementById("switch").backgroundImage = "images/moon.jpeg";
         document.getElementById("Resume").src = "images/resumelogo.png";
+        document.getElementById("Mail").src = "images/mail_black.png";
         document.getElementById("GitHub").src = "images/githublogo2.png";
         document.getElementById("wca").src = "images/rubik_dark.png";
         document.getElementById("tab1").style.color = "white";
         document.getElementById("tab2").style.color = "white";
+        document.getElementById("button").style.backgroundImage = "url('images/menu_white.png')";
+        document.getElementById("button").style.backgroundColor = "black";
+    }
+}
+
+function lightModeProject() {
+    var target1 = document.body;
+    target1.classList.toggle("dark-mode");
+    if (document.getElementById("switch").checked === true) {
+        document.getElementById("tab1").style.color = "black";
+        document.getElementById("tab2").style.color = "black";
+        document.getElementById("button").style.backgroundImage = "url('images/menu_black.png')";
+        document.getElementById("button").style.backgroundColor = "white";
+    } else {
+        document.getElementById("tab1").style.color = "white";
+        document.getElementById("tab2").style.color = "white";
+        document.getElementById("button").style.backgroundImage = "url('images/menu_white.png')";
+        document.getElementById("button").style.backgroundColor = "black";
+    }
+}
+
+function showTabs() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }
